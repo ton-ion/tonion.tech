aa6880d5c0ef23f76f84cb771cd9e5e6d57313a2
saghar <saghar.raadco@gmail.com>
2024-07-19 20:35:30 +0330

    feat(Home): add HomepageFeatures2 and OurSponsors section

---
+++ src/components/Home/OurSponsors/index.tsx
@@ -0,0 +1,20 @@
+
+
+export default function OurSponsors(): JSX.Element {
+    return(
+<div className="max-w-3xl mx-auto px-5 my-10">
+  <div className="flex flex-col justify-center">
+    <div className="text-center">
+      <h2 className="font-semibold text-3xl">Our Sponsors</h2>
+      <p className="max-w-md mx-auto mt-2 text-gray-500">
+        We are thankful to each and every company sponsored our plugin which
+        helped us to continue working on it.
+      </p>
+    </div>
+    <div className="flex flex-wrap items-center justify-center gap-10 mt-2 md:justify-around">
+      <div className="text-gray-400 ">Logo</div>
+    </div>
+  </div>
+</div>
+    )
+}
